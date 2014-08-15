55d9c8a31723501883c94eecedfcb020c8657515
Ten Mutunhire <tmutunhire@gmail.com>
2014-08-15 18:56:41 -0400

    logs

---
+++ js/local.js
@@ -0,0 +1,16 @@
+alert("initializing our app...")
+google.load("earth", "1", {"other_params": "sensor={true}"});
+
+function init() {
+    console.log("init.d");
+    google.earth.createInstance('map3d', initCB, failureCB);
+}
+
+function initCB(instance) {
+    ge = instance;
+    ge.getWindow().setVisibility(true);
+}
+
+function failureCB(errorCode) {
+    alert("failed with error code" + errorCode.toString());
+}
