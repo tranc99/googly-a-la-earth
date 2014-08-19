1aed636a660bc1ea37018a18770f848a7e8f65f8
Ten Mutunhire <tmutunhire@gmail.com>
2014-08-18 22:00:25 -0400

    add placemarks

--- js/local.js
+++ js/local.js
@@ -3,17 +3,6 @@ google.load("earth", "1", {"other_params": "sensor={false}"});
 
 var ge;
 
-/*function createPlacemarks() {
-    var placemark = ge.createPlacemark('');
-
-    var point = ge.createPoint('');
-    point.setLatitude(12.345);
-    point.setLongitude(54.321);
-    placemark.setGeometry(point);
-
-    ge.getFeatures().appendChild(placemark);
-}*/
-
 function failureCB(errorCode) {
     alert("failed with error code" + errorCode.toString());
 }
@@ -35,13 +24,27 @@ function initCB(instance) {
     var placemark = ge.createPlacemark('');
     placemark.setName("AT&T");
 
-    //define a custom icon
-    var icon = ge.createIcon('');
-    icon.setHref('http://maps.google.com/mapfiles/kml/paddle/red-circle.png');
-    var style = ge.createStyle(''); //create a new style
-    style.getIconStyle().setIcon(icon); //apply the icon to the style
-    style.getIconStyle().setScale(5.0);
-    placemark.setStyleSelector(style); //apply the style to the placemark
+    //create a style map
+    var styleMap = ge.createStyleMap('');
+
+    //create normal style for the style map
+    var normalStyle = ge.createStyle('');
+    var normalIcon = ge.createIcon('');
+    normalIcon.setHref('http://maps.google.com/mapfiles/kml/paddle/red-circle.png');
+    normalStyle.getIconStyle().setIcon(normalIcon);
+
+    //create highlight style for style map
+    var highlightStyle = ge.createStyle('');
+    var highlightIcon = ge.createIcon('');
+    highlightIcon.setHref('http://google-maps-icons.googlecode.com/files/girlfriend.png');
+    highlightStyle.getIconStyle().setIcon(highlightIcon);
+    highlightStyle.getIconStyle().setScale(5.0);
+
+    styleMap.setNormalStyle(normalStyle);
+    styleMap.setHighlightStyle(highlightStyle);
+
+    //apply styleMap to a placemark
+    placemark.setStyleSelector(styleMap);
 
     // Set the placemark's location.  
     var point = ge.createPoint('');
