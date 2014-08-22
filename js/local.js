f68da312f9c1fd0d376abb53be8283077ae976b2
Ten Mutunhire <tmutunhire@gmail.com>
2014-08-21 23:00:17 -0400

    Add sunshine information

--- js/local.js
+++ js/local.js
@@ -17,6 +17,7 @@ function init() {
 
 function initCB(instance) {
     ge = instance;
+    ge.getSun().setVisibility(true);
     ge.getWindow().setVisibility(true);
     ge.getNavigationControl().setVisibility(ge.VISIBILITY_SHOW);
 
@@ -24,35 +25,21 @@ function initCB(instance) {
     la.set(48.761, -121.794, 0, ge.ALTITUDE_RELATIVE_TO_GROUND, -8.541, 66.213, 20000);
     ge.getView().setAbstractView(la);
 
-    // Create the placemark.
-    var lineStringPlacemark = ge.createPlacemark('');
+    // Create the GroundOverlay.
+    var groundOverlay = ge.createGroundOverlay('');
 
-    // Create the LineString; set it to extend down to the ground
-    // and set the altitude mode.
-    var lineString = ge.createLineString('');
-    lineStringPlacemark.setGeometry(lineString);
-    lineString.setExtrude(true);
-    lineString.setAltitudeMode(ge.ALTITUDE_RELATIVE_TO_GROUND);
+    // Specify the image path and assign it to the GroundOverlay.
+    var icon = ge.createIcon('');
+    icon.setHref("http://www.google.com/logos/earthday08.gif");
+    groundOverlay.setIcon(icon);
 
-    // Add LineString points.
-    lineString.getCoordinates().pushLatLngAlt(48.754, -121.835, 700);
-    lineString.getCoordinates().pushLatLngAlt(48.764, -121.828, 700);
-    lineString.getCoordinates().pushLatLngAlt(48.776, -121.818, 700);
-    lineString.getCoordinates().pushLatLngAlt(48.787, -121.794, 700);
-    lineString.getCoordinates().pushLatLngAlt(48.781, -121.778, 700);
-    lineString.getCoordinates().pushLatLngAlt(48.771, -121.766, 700);
-    lineString.getCoordinates().pushLatLngAlt(48.757, -121.768, 700);
-    lineString.getCoordinates().pushLatLngAlt(48.747, -121.773, 700);
+    // Specify the geographic location.
+    var latLonBox = ge.createLatLonBox('');
+    latLonBox.setBox(48.80, 48.75, -121.77, -121.85, 0);
+    groundOverlay.setLatLonBox(latLonBox);
 
-    // Create a style and set width and color of line.
-    lineStringPlacemark.setStyleSelector(ge.createStyle(''));
-    var lineStyle = lineStringPlacemark.getStyleSelector().getLineStyle();
-    lineStyle.setWidth(5);
-    lineStyle.getColor().set('9900ffff');  // aabbggrr format
-
-    // Add the feature to Earth.
-    ge.getFeatures().appendChild(lineStringPlacemark);    
-
+    // Add the GroundOverlay to Earth.
+    ge.getFeatures().appendChild(groundOverlay);
 }
 
 
